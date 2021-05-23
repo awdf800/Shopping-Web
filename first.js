0274575b2ba3f67f1a153e11e1785f1ed465d163
awdf800 <84678022+awdf800@users.noreply.github.com>
2021-05-24 01:18:32 +0800

    Add files via upload

---
+++ first.js
@@ -0,0 +1,47 @@
+﻿var i = 1;
+$(".shopingCar .add a").click(function () {
+    if (i == 0) {
+        $(".shopingCar").animate({ right: "0px" }, 500);
+        i = 1;
+    } else if (i == 1) {
+        $(".shopingCar").animate({ right: "-540px" }, 500);
+        window.setTimeout("window.location='index.html'", 500);
+        i = 0;
+    }
+});
+
+$(".thing_list ul li p a").click(function () {
+    $(this).css("background", "green");
+
+    var img = $(this).parent().siblings("img").attr("src");
+    var txt = $(this).parent().siblings("p.top_name").html();
+    var money = $(this).siblings("span").html();
+    $(".lieb").append("<blockquote><blockquote><img style='float:left' src=' " + img + "width='150' height='150'  /> <p style='float:left'>" + txt + "</p><p style='float:right'>" + money + "</p></blockquote></blockquote>");
+        play(event);
+});
+function play(event){
+     var s_left=event.clientX;
+     var s_top=event.clientY;
+     var e_left=$(".shopingCar .add a").offset().left;
+     var e_top=$(".shopingCar .add a").offset().top;
+     var _this=$(event.target);
+     var img=_this.parent().siblings("img").attr("src");
+     var flying= $("<img src='" + img + "' width='50' style='border-radius:50%'/>");
+    flying.fly({
+        start: {
+            left: s_left,
+            top: s_top
+        },
+
+        end: {
+            left: e_left,
+            top: e_top
+        },
+
+        onEnd: function () {
+            flying.fadeOut("slow", function () {
+                $(this).remove();
+            });
+        }
+    })
+}
---
+++ main.js
@@ -0,0 +1,66 @@
+﻿// 此處為您的程式碼!
+var shopWeb2= new Vue({
+    el: "#shopWeb2",
+    data: {
+        itemList: [
+            {
+                id: "1",
+                itemName: "RTX2060",
+                imgUrl: "https://i.imgur.com/4PXvJND.png",
+                price: "13000",
+                count: "1"
+            },
+            {
+                id: "2",
+                itemName: "RTX2070",
+                imgUrl: "https://i.imgur.com/uUMENQC.png",
+                price: "18000",
+                count: "1"
+            },
+            {
+                id: "3",
+                itemName: "RTX2080",
+                imgUrl: "https://i.imgur.com/O8F6N5h.png",
+                price: "22000",
+                count: "1"
+            },
+            {
+                id: "4",
+                itemName: "RTX2080Ti",
+                imgUrl: "https://i.imgur.com/16cCcv8.png",
+                price: "28000",
+                count: "1"
+            },
+        ]
+    },
+    methods: {
+        buttonPlus: function (item) {
+            console.log(item);
+            item.count++;
+        },
+        buttonSub: function (item) {
+            console.log(item);
+            if (item.count > 1) {
+                item.count--;
+            }
+        },
+        buttonDelete: function (index) {
+            this.itemList.splice(index, 1);
+        }
+    },
+    computed: {
+
+    }
+})
+
+var i = 1;
+$(".Return .turn a").click(function () {
+    if (i == 0) {
+        $(".Return").animate({ left: "-5px" }, 500);
+        i = 1;
+    } else if (i == 1) {
+        $(".Return").animate({ left: "-440px" }, 500);
+        window.setTimeout("window.location='page.html' ", 500);
+        i = 0;
+    }
+});
